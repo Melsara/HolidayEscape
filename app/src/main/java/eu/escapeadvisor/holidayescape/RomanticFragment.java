e04569607cdcc12f229a67387fc488e3a89a76f6
Melsara <kaxhortz@gmail.com>
2018-04-03 22:34:09 +0200

    Adding new fragments.

--- app/src/main/java/eu/escapeadvisor/holidayescape/RomanticFragment.java
+++ app/src/main/java/eu/escapeadvisor/holidayescape/RomanticFragment.java
@@ -1,7 +1,7 @@
 package eu.escapeadvisor.holidayescape;
 
-
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,18 +9,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class BarsFragment extends Fragment {
-
+public class RomanticFragment extends Fragment {
     URL url1 = new URL("http://www.google.com/");
     Context mContext;
 
-
-    public BarsFragment() throws MalformedURLException {
+    public RomanticFragment() throws MalformedURLException {
         // Required empty public constructor
     }
 
@@ -28,17 +27,17 @@ public class BarsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        mContext = getContext();
+        mContext  = getContext();
         View rootView = inflater.inflate(R.layout.activity_poi_list, container, false);
-        final ArrayList<POI> bars = new ArrayList<>();
+        final ArrayList<POI> romantics = new ArrayList<>();
 
-        bars.add(new POI(mContext.getResources().getString(R.string.category_bars),"Bar1", url1, "email", "000000000",
-                        0, 0, R.drawable.ic_launcher_background, "Lorem ipsum","address" ));
+        romantics.add(new POI(mContext.getResources().getString(R.string.category_romanticPlaces),"Romantic1", url1, "email", "000000000",
+                0, 0, R.drawable.ic_launcher_background, "Lorem ipsum","address" ));
 
-        PoiAdapter barsAdapter = new PoiAdapter(getActivity(), bars);
+        PoiAdapter romanticsAdapter = new PoiAdapter(getActivity(), romantics);
         ListView listView = (ListView) rootView.findViewById(R.id.list);
 
-        listView.setAdapter(barsAdapter);
+        listView.setAdapter(romanticsAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
