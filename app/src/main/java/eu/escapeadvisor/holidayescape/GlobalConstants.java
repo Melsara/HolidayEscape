a71ac561455785d10c9aaab738f22d62c80d33f6
Melsara <kaxhortz@gmail.com>
2018-04-04 22:36:35 +0200

    Initiating intents on Bars item list.

---
+++ app/src/main/java/eu/escapeadvisor/holidayescape/GlobalConstants.java
@@ -0,0 +1,13 @@
+package eu.escapeadvisor.holidayescape;
+
+public interface GlobalConstants {
+    String URL_KEY = "url_key";
+    String NAME_KEY = "name_key";
+    String EMAIL_KEY = "email_key";
+    String TEL_KEY = "tel_key";
+    String LAT_KEY = "lat_key";
+    String LNG_KEY = "lng_key";
+    String IMAGE_KEY = "image_key";
+    String DESC_KEY = "description_key";
+    String ADDRESS_KEY = "address_key";
+}
