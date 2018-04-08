59b6f65366a05e6c7902cbefb4d279caedbf3e2c
Melsara <kaxhortz@gmail.com>
2018-04-08 21:52:16 +0200

    Adding first er.

--- app/src/main/java/eu/escapeadvisor/holidayescape/ErFragment.java
+++ app/src/main/java/eu/escapeadvisor/holidayescape/ErFragment.java
@@ -2,6 +2,7 @@ package eu.escapeadvisor.holidayescape;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -30,8 +31,8 @@ public class ErFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.activity_poi_list, container, false);
         final ArrayList<POI> ers = new ArrayList<>();
 
-        ers.add(new POI(mContext.getResources().getString(R.string.category_er),"ER1", "url", "email", "000000000",
-                0, 0, R.drawable.ic_launcher_background, "Lorem ipsum","address" ));
+        ers.add(new POI(mContext.getResources().getString(R.string.category_er),getString(R.string.er_name1), getString(R.string.er_url1), getString(R.string.er_email1), getString(R.string.er_tel1),
+                39.215552, 9.118157, R.drawable.entrapment, getString(R.string.er_desc1),getString(R.string.er_address1) ));
 
         PoiAdapter ersAdapter = new PoiAdapter(getActivity(), ers);
         ListView listView = (ListView) rootView.findViewById(R.id.list);
@@ -41,7 +42,32 @@ public class ErFragment extends Fragment {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                POI er = (POI) adapterView.getItemAtPosition(i);
+                String url = er.getUrl();
+                String category = er.getCategory();
+                String name = er.getName();
+                String email = er.getEmail();
+                String tel = er.getTel();
+                double lat = er.getLat();
+                double lng = er.getLng();
+                int image = er.getImage();
+                String description = er.getDescription();
+                String address = er.getAddress();
 
+                Intent openBarDetail = new Intent(getContext(), DetailsActivity.class);
+
+                Bundle bundle = new Bundle();
+                bundle.putString(GlobalConstants.URL_KEY, url);
+                bundle.putString(GlobalConstants.NAME_KEY, name);
+                bundle.putString(GlobalConstants.EMAIL_KEY, email);
+                bundle.putString(GlobalConstants.TEL_KEY, tel);
+                bundle.putDouble(GlobalConstants.LAT_KEY, lat);
+                bundle.putDouble(GlobalConstants.LNG_KEY, lng);
+                bundle.putInt(GlobalConstants.IMAGE_KEY, image);
+                bundle.putString(GlobalConstants.DESC_KEY, description);
+                bundle.putString(GlobalConstants.ADDRESS_KEY, address);
+                openBarDetail.putExtras(bundle);
+                startActivity(openBarDetail);
             }
         });
 
