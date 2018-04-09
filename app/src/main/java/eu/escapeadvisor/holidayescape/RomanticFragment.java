6b37a9a66a52fc65fe5f680b31919281563e0640
Melsara <kaxhortz@gmail.com>
2018-04-09 21:30:37 +0200

    Adding first romantic place.

--- app/src/main/java/eu/escapeadvisor/holidayescape/RomanticFragment.java
+++ app/src/main/java/eu/escapeadvisor/holidayescape/RomanticFragment.java
@@ -1,6 +1,7 @@
 package eu.escapeadvisor.holidayescape;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -30,8 +31,8 @@ public class RomanticFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.activity_poi_list, container, false);
         final ArrayList<POI> romantics = new ArrayList<>();
 
-        romantics.add(new POI(mContext.getResources().getString(R.string.category_romanticPlaces),"Romantic1", "url", "email", "000000000",
-                0, 0, R.drawable.ic_launcher_background, "Lorem ipsum","address" ));
+        romantics.add(new POI(mContext.getResources().getString(R.string.category_romanticPlaces),getString(R.string.rom_name1), getString(R.string.rom_url1), getString(R.string.rom_email1), getString(R.string.rom_tel1),
+                39.234325, 9.115732, R.drawable.monteclaro, getString(R.string.rom_desc1),getString(R.string.rom_address1) ));
 
         PoiAdapter romanticsAdapter = new PoiAdapter(getActivity(), romantics);
         ListView listView = (ListView) rootView.findViewById(R.id.list);
@@ -41,6 +42,32 @@ public class RomanticFragment extends Fragment {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                POI romantics = (POI) adapterView.getItemAtPosition(i);
+                String url = romantics.getUrl();
+                String category = romantics.getCategory();
+                String name = romantics.getName();
+                String email = romantics.getEmail();
+                String tel = romantics.getTel();
+                double lat = romantics.getLat();
+                double lng = romantics.getLng();
+                int image = romantics.getImage();
+                String description = romantics.getDescription();
+                String address = romantics.getAddress();
+
+                Intent openBarDetail = new Intent(getContext(), DetailsActivity.class);
+
+                Bundle bundle = new Bundle();
+                bundle.putString(GlobalConstants.URL_KEY, url);
+                bundle.putString(GlobalConstants.NAME_KEY, name);
+                bundle.putString(GlobalConstants.EMAIL_KEY, email);
+                bundle.putString(GlobalConstants.TEL_KEY, tel);
+                bundle.putDouble(GlobalConstants.LAT_KEY, lat);
+                bundle.putDouble(GlobalConstants.LNG_KEY, lng);
+                bundle.putInt(GlobalConstants.IMAGE_KEY, image);
+                bundle.putString(GlobalConstants.DESC_KEY, description);
+                bundle.putString(GlobalConstants.ADDRESS_KEY, address);
+                openBarDetail.putExtras(bundle);
+                startActivity(openBarDetail);
 
             }
         });
